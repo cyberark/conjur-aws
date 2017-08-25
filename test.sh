5bba0a6f594a79e7585ff451ce354f03c4ce3d53
Alan Potter <alanjpotter@gmail.com>
2017-08-25 09:00:28 -0400

    retry health check, always clean up

--- test.sh
+++ test.sh
@@ -6,4 +6,9 @@ set -a
 AMI_ID=$(< ami-id.out)
 : ${STACK_NAME=conjur-ce-test-$(date +%s)}
 
+finish() {
+  ./ansible.sh ansible-playbook -e stack_name=${STACK_NAME} -e terminate_after_test=true -v test.yml
+}
+trap finish EXIT
+  
 ./ansible.sh ${@-ansible-playbook -e stack_name=${STACK_NAME} -v test.yml}
