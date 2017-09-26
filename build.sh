59b758cdc089f217d78a5914b943450be1140902
Dustin Collins <dustinrcollins@gmail.com>
2017-09-26 12:30:11 -0500

    export CONJUR_VERSION in build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -eux
 
+export CONJUR_VERSION="${1:-latest}"
+
 # Default these variables to make development easier. They should be get in the environment (i.e. in the Jenkinsfile)
 # when this script gets used as part of a build.
 : ${INSTANCE_ID_FILE=dev-EC2.txt}
