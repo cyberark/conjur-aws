bdf716c49bfd0b5d8b21b0b7995d2cb0deb05175
Dustin Collins <dustinrcollins@gmail.com>
2017-10-02 12:05:34 -0500

    Use include_vars to pull in AMIs

--- render-cft.sh
+++ render-cft.sh
@@ -2,6 +2,4 @@
 
 # Render newly created AMI into CloudFormation template, for testing
 
-./ansible.sh ansible-playbook -v \
-  -e "@vars-amis.yml" \
-  render-cft.yml
+./ansible.sh ansible-playbook -v render-cft.yml
