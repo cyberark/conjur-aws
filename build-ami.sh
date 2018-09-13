d4c8d446c174431b529391d4311048db122e6934
Gary Moon <gary@garymoon.net>
2018-09-13 15:46:34 -0400

    Add logic to fetch latest CoreOS AMI

--- build-ami.sh
+++ build-ami.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -eu
+#!/bin/bash -euo pipefail
 
 CONJUR_VERSION="${1:-latest}"
 # Default these variables to make development easier. They should be get in the environment (i.e. in the Jenkinsfile)
@@ -18,8 +18,29 @@ trap finish EXIT
 
 mkdir -p vars  # variables passed between plays are stored here
 
+echo "Fetching latest CoreOS AMI..."
+BASE_AMI=$(docker run --rm --env-file ansible.env \
+  mesosphere/aws-cli ec2 describe-images --filters '[
+    {"Name": "owner-id", "Values": ["595879546273"] },
+    {"Name": "name", "Values": ["CoreOS-stable*"] },
+    {"Name": "virtualization-type", "Values": ["hvm"] },
+    {"Name": "architecture", "Values": ["x86_64"] },
+    {"Name": "hypervisor", "Values": ["xen"] },
+    {"Name": "root-device-type", "Values": ["ebs"] },
+    {"Name": "state", "Values": ["available"] }
+    ]' \
+    --query 'reverse(sort_by(Images, &CreationDate))[:1].ImageId | [0]' \
+    --region $AWS_REGION \
+    --output text
+  )
+
+echo "CoreOS AMI: $BASE_AMI"
+
+echo "Starting build"
+
 ./ansible.sh ansible-playbook -v \
   -e ami_id_filename="${AMI_ID_FILE}" \
   -e instance_id_filename="${INSTANCE_ID_FILE}" \
   -e conjur_version="$CONJUR_VERSION" \
+  -e BASE_AMI="$BASE_AMI" \
   build-ami.yml
